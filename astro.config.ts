aa3036729420356b81489f7e91235c092e13360d
syamsul <muhamadsyamsulhalim@gmail.com>
2026-08-28 19:09:30 +0700

    fix/fixing-theme-and-astr0-config

--- astro.config.ts
+++ astro.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import lotus from '@prosefly/astro-theme-lotus';
 
 export default defineConfig({
-  integrations: [lotus()],
+  site: 'https://acuy123-off.github.io',
+  base: '/mugen-guide',
+  // ... leave your existing integrations (like lotus or mdx) here
 });
